1d281fce7b0d94f97df963f571490cc72c40dc42
Nischal Valluru <Diamond8487@outlook.com>
2025-09-12 17:12:21 +0100

    Delete simple_age_calculator.py

--- simple_age_calculator.py
+++
@@ -1,4 +0,0 @@
-birth_year = int(input("What year were you born in? "))
-current_year = int(input("What year are you currently in? "))
-age = current_year - birth_year
-print(f"By the end of {current_year}, you will be {age} years old!")
